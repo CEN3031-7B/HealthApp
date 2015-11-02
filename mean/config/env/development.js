e41e4b5f377dadf84f03ac6e6a51814b09448f94
mbentz <mbentz@ufl.edu>
2015-11-02 15:35:41 -0500

    Updated webpage title and icon

--- mean/config/env/development.js
+++ mean/config/env/development.js
@@ -22,7 +22,7 @@ module.exports = {
     }
   },
   app: {
-    title: defaultEnvConfig.app.title + ' - Development Environment'
+    title: defaultEnvConfig.app.title + ' - Helping the local community'
   },
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'APP_ID',
