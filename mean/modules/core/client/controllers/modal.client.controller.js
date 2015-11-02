d7eac0c67ca1e1135789bd4e05a0725158d79c81
mbentz <mbentz@ufl.edu>
2015-11-02 15:03:34 -0500

    Added email functionality to modal controller

--- mean/modules/core/client/controllers/modal.client.controller.js
+++ mean/modules/core/client/controllers/modal.client.controller.js
@@ -1,9 +1,7 @@
 'use strict';
 
-angular.module('core').controller('ModalController', ['$scope','$modal' ,'$http', '$log', '$state',
-  function ($scope, $modal, $http, $log, $state) {
-
-    $("modal-content").load("/manage.client.view.html");
+angular.module('core').controller('ModalController', ['$scope','$modal' ,'$http', '$log','$window','$location',
+  function ($scope, $modal, $http, $log, $window, $location) {
 
   $scope.animationsEnabled = true;
 
@@ -13,20 +11,25 @@ angular.module('core').controller('ModalController', ['$scope','$modal' ,'$http'
         templateUrl: id,
         controller: function ($scope, $modalInstance) {
           $scope.cancel = function () {
+            console.log("canceling");
             $modalInstance.dismiss('cancel');
+          };
+
+          $scope.sendMail = function(isValid) {
+            $scope.error = null;
+
+            if (!isValid) {
+              $scope.$broadcast('show-errors-check-validity', 'userForm');
+              return false;
+            }
+            var link = "mailto:me@example.com"
+                     + "?subject=" + $scope.message.user + " is concerned!"
+                     + "&body=" + $scope.message.text;
+            window.location.href = link;
           };
         },
         size: size
       });
-    };
-
-    $scope.sendMail = function(isValid) {
-      $scope.error = null;
-
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
-        return false;
-      }
     };
   }
 ]);
